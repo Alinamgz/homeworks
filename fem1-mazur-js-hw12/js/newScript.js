ee3c7313b64d0379ec0146fb4dd11d29647a5e93
AlinaMgz <alinamgz@ukr.net>
2019-06-02 00:32:27 +0300

    @AlinaMgz fixed js hw 12

---
+++ fem1-mazur-js-hw12/js/newScript.js
@@ -0,0 +1,51 @@
+// Dig out needed imgs
+const imgNL  = document.querySelectorAll("img");
+
+// main part
+let counter = 1;
+let check = true;
+
+const imgVisibility = () => {
+
+    imgNL.forEach((img, i) => {
+        if (counter === i) {
+            img.style.visibility = 'visible'
+            img.style.order = '0'
+        } else {
+            img.style.visibility= 'hidden'
+            img.style.order = '1'
+        }
+    });
+
+    if (counter < 3){
+        counter++
+    } else {
+        counter = 0
+    }
+};
+
+let imgVisibilityCycle = setInterval(imgVisibility,500);
+
+// Stop-Btn
+const stopBtn = document.createElement("button");
+stopBtn.className = 'stop-btn';
+stopBtn.innerText = "STOP!"
+document.body.appendChild(stopBtn);
+
+stopBtn.addEventListener("click", () => {
+    clearInterval(imgVisibilityCycle);
+    check = false;
+});
+
+// Continue-Btn
+const continueBtn = document.createElement("button");
+continueBtn.className = 'continue-btn';
+continueBtn.innerText = 'want more';
+document.body.appendChild(continueBtn);
+
+continueBtn.addEventListener("click", () => {
+    if (check === false) {
+        imgVisibilityCycle = setInterval(imgVisibility , 500);
+        check = true
+    };
+});
