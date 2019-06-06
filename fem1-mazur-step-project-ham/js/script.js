1d9982bc50dfb2a5955430a2338e6f688c623045
AlinaMgz <alinamgz@ukr.net>
2019-06-06 20:59:30 +0300

    @alinamgz header, options: done. Started services

--- fem1-mazur-step-project-ham/js/script.js
+++ fem1-mazur-step-project-ham/js/script.js
@@ -3,3 +3,10 @@ const menuHover = $('.menu-itm-color');
 menuHover.hover(function(){
     $(this).toggleClass('green-txt underline');
 });
+
+//tabs for services
+$('.services-tabbar-itm').on('click', (e) => {
+    const dataValue = $(e.target).data('content');
+    $('.tabs-content-itm').hide();
+    $('.tabs-content-paarent').find(`[data-content = ${dataValue}]`).show();
+});
