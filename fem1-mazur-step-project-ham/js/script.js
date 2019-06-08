9402cc78f7dd703d098e5213342aa63db4bb452c
AlinaMgz <alinamgz@ukr.net>
2019-06-08 23:19:15 +0300

    @alinamgz finished purchases

--- fem1-mazur-step-project-ham/js/script.js
+++ fem1-mazur-step-project-ham/js/script.js
@@ -1,12 +1,20 @@
 // hover for header menu
 const menuHover = $('.menu-itm-color');
 menuHover.hover(function(){
-    $(this).toggleClass('green-txt underline');
+    return $(this).toggleClass('green-txt underline');
 });
 
 //tabs for services
-$('.services-tabbar-itm').on('click', (e) => {
+const activeTab = $('.tabbar-itm');
+
+activeTab.on('click', function (e) {
+
+    //adding decoration for active tab
+    activeTab.removeClass('tabbar-active');
+    $(this).addClass ('tabbar-active' );
+
+    //calling content that belongs to this tab
     const dataValue = $(e.target).data('content');
     $('.tabs-content-itm').hide();
-    $('.tabs-content-paarent').find(`[data-content = ${dataValue}]`).show();
+    $('.tabs-content-parent').find(`[data-content = ${dataValue}]`).show();
 });
