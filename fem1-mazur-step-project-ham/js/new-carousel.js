1c0bf806b7a9dcc5638cb47cfd0d05c0c3943698
AlinaMgz <alinamgz@ukr.net>
2019-06-20 19:45:47 +0300

    @alinamgz wooho, seems like new carousel is working! now i have to delete all useless lines and add some animation.

---
+++ fem1-mazur-step-project-ham/js/new-carousel.js
@@ -0,0 +1,86 @@
+// // // what people say carousel v2.  trying to rewrite it on jQuery
+//
+const sliderBox = $('.sliderbox');
+const slides = $('.wps-slider-itm-wrapper');
+let slidesAmount = slides.length;
+let currentSlide = slides.eq(0);
+console.log(slides);
+let counter = 0;
+// arrows
+const next = $('.next');
+const prev = $('.previous');
+//miniatures
+const miniatures = $('.slider-nav-img');
+const miniaturesAmount = miniatures.length;
+let currentMiniature = miniatures.eq(0);
+
+//miniatures controls
+
+
+miniatures.on('click', function(){
+    miniatures.removeClass('slider-nav-img-active');
+    $(this).addClass('slider-nav-img-active');
+
+    counter =$(this).index()-1;
+
+    slides.removeClass('wps-current');
+    currentSlide = slides.eq(counter);
+    currentSlide.addClass('wps-current');
+});
+
+// function findInd (ev) {
+//     let target = $(ev.target);
+//     let number = (target.index());
+//
+//     counter = number -1;
+//
+//     currentSlide = slides.eq(counter);
+//     slides.removeClass('wps-current');
+//     currentSlide.addClass('wps-current')
+// }
+//
+// miniatures.on('click', findInd);
+
+
+(function ($) {
+    $.fn.carousel = function () {
+        const navigate = function(direction){
+
+            slides.removeClass('wps-current');
+            miniatures.removeClass('slider-nav-img-active');
+
+            counter += direction;
+
+            if (direction === -1 && counter <0 && slidesAmount === miniaturesAmount){
+                counter += slidesAmount - 1;
+            }
+            if (direction === 1 && !slides[counter] && !miniatures[counter]){
+                counter = 0;
+            }
+
+            // if (direction > 1){
+            //     counter = direction
+            // }
+
+            // let miniControl = function (e) {
+            //     miniatures.each(function (index) {
+            //         if (index === $(e.target)){
+            //             return counter = index
+            //         }
+            //     });
+            // };
+
+            currentSlide = slides.eq(counter);
+            currentSlide.addClass('wps-current');
+
+            currentMiniature = miniatures.eq(counter);
+            currentMiniature.addClass('slider-nav-img-active');
+
+        };
+
+        next.on('click',function(){navigate(1)});
+        prev.on('click', function () {navigate(-1)});
+    }
+}(jQuery));
+sliderBox.carousel();
+
--- fem1-mazur-step-project-ham/js/script.js
+++ fem1-mazur-step-project-ham/js/script.js
@@ -58,106 +58,106 @@ const showImgs = () => {
 
 workBtn.addEventListener('click', showImgs);
 
-
-// What people say carousel
-
-const sliderBox = document.querySelector('.sliderbox');
-const slides = document.querySelectorAll('.wps-slider-itm-wrapper');
-let slidesCounter = 0;
-const amount = slides.length;
-let currentSlide = slides[0];
-// arrows
-const next = document.querySelector('.next');
-const prev = document.querySelector('.previous');
-//miniatures
-const ava = document.getElementById('ava');
-const andy = document.getElementById('andy');
-const hasan = document.getElementById('hasan');
-const jennifer = document.getElementById('jennifer');
-
-carousel = (function(){
-    function arrowNavigate(direction){
-        // arrows controls
-        currentSlide.classList.remove('wps-current');
-        slidesCounter = slidesCounter + direction;
-        if (direction === -1 &&
-            slidesCounter < 0) {
-            slidesCounter = amount - 1;
-        }
-        if (direction === 1 &&
-            !slides[slidesCounter]) {
-            slidesCounter = 0;
-        }
-        // for miniatures controls
-        switch (direction) {
-            case 'ava':
-                slidesCounter = 0;
-                break;
-            case 'andy':
-                slidesCounter = 1;
-                break;
-            case 'hasan':
-                slidesCounter = 2;
-                break;
-            case 'jennifer':
-                slidesCounter = 3;
-                break
-        }
-
-        // decoration for active miniature
-        switch (slidesCounter) {
-            case 0:
-                ava.classList.add('slider-nav-img-active');
-                andy.classList.remove('slider-nav-img-active');
-                hasan.classList.remove('slider-nav-img-active');
-                jennifer.classList.remove('slider-nav-img-active');
-                break;
-            case 1:
-                andy.classList.add('slider-nav-img-active');
-                hasan.classList.remove('slider-nav-img-active');
-                jennifer.classList.remove('slider-nav-img-active');
-                ava.classList.remove('slider-nav-img-active');
-                break;
-            case 2:
-                hasan.classList.add('slider-nav-img-active');
-                jennifer.classList.remove('slider-nav-img-active');
-                ava.classList.remove('slider-nav-img-active');
-                andy.classList.remove('slider-nav-img-active');
-                break;
-            case 3:
-                jennifer.classList.add('slider-nav-img-active');
-                ava.classList.remove('slider-nav-img-active');
-                andy.classList.remove('slider-nav-img-active');
-                hasan.classList.remove('slider-nav-img-active');
-                break
-        }
-
-        currentSlide = slides[slidesCounter];
-        currentSlide.classList.add('wps-current');
-    }
-
-    next.addEventListener('click', function() {
-        arrowNavigate(1);
-    });
-
-    prev.addEventListener('click', function() {
-        arrowNavigate(-1);
-    });
-    arrowNavigate(0);
-
-    ava.addEventListener('click', function () {
-        arrowNavigate('ava')
-    });
-    andy.addEventListener('click', function () {
-        arrowNavigate('andy')
-
-    });
-    hasan.addEventListener('click', function () {
-        arrowNavigate('hasan')
-
-    });
-    jennifer.addEventListener('click', function () {
-        arrowNavigate('jennifer')
-    });
-})();
-
+//
+// // What people say carousel
+//
+// const sliderBox = document.querySelector('.sliderbox');
+// const slides = document.querySelectorAll('.wps-slider-itm-wrapper');
+// let slidesCounter = 0;
+// const amount = slides.length;
+// let currentSlide = slides[0];
+// // arrows
+// const next = document.querySelector('.next');
+// const prev = document.querySelector('.previous');
+// //miniatures
+// const ava = document.getElementById('ava');
+// const andy = document.getElementById('andy');
+// const hasan = document.getElementById('hasan');
+// const jennifer = document.getElementById('jennifer');
+//
+// const carousel = (function(){
+//     function arrowNavigate(direction){
+//         // arrows controls
+//         currentSlide.classList.remove('wps-current');
+//         slidesCounter = slidesCounter + direction;
+//         if (direction === -1 &&
+//             slidesCounter < 0) {
+//             slidesCounter = amount - 1;
+//         }
+//         if (direction === 1 &&
+//             !slides[slidesCounter]) {
+//             slidesCounter = 0;
+//         }
+//         // for miniatures controls
+//         switch (direction) {
+//             case 'ava':
+//                 slidesCounter = 0;
+//                 break;
+//             case 'andy':
+//                 slidesCounter = 1;
+//                 break;
+//             case 'hasan':
+//                 slidesCounter = 2;
+//                 break;
+//             case 'jennifer':
+//                 slidesCounter = 3;
+//                 break
+//         }
+//
+//         // decoration for active miniature
+//         switch (slidesCounter) {
+//             case 0:
+//                 ava.classList.add('slider-nav-img-active');
+//                 andy.classList.remove('slider-nav-img-active');
+//                 hasan.classList.remove('slider-nav-img-active');
+//                 jennifer.classList.remove('slider-nav-img-active');
+//                 break;
+//             case 1:
+//                 andy.classList.add('slider-nav-img-active');
+//                 hasan.classList.remove('slider-nav-img-active');
+//                 jennifer.classList.remove('slider-nav-img-active');
+//                 ava.classList.remove('slider-nav-img-active');
+//                 break;
+//             case 2:
+//                 hasan.classList.add('slider-nav-img-active');
+//                 jennifer.classList.remove('slider-nav-img-active');
+//                 ava.classList.remove('slider-nav-img-active');
+//                 andy.classList.remove('slider-nav-img-active');
+//                 break;
+//             case 3:
+//                 jennifer.classList.add('slider-nav-img-active');
+//                 ava.classList.remove('slider-nav-img-active');
+//                 andy.classList.remove('slider-nav-img-active');
+//                 hasan.classList.remove('slider-nav-img-active');
+//                 break
+//         }
+//
+//         currentSlide = slides[slidesCounter];
+//         currentSlide.classList.add('wps-current');
+//     }
+//
+//     next.addEventListener('click', function() {
+//         arrowNavigate(1);
+//     });
+//
+//     prev.addEventListener('click', function() {
+//         arrowNavigate(-1);
+//     });
+//     arrowNavigate(0);
+//
+//     ava.addEventListener('click', function () {
+//         arrowNavigate('ava')
+//     });
+//     andy.addEventListener('click', function () {
+//         arrowNavigate('andy')
+//
+//     });
+//     hasan.addEventListener('click', function () {
+//         arrowNavigate('hasan')
+//
+//     });
+//     jennifer.addEventListener('click', function () {
+//         arrowNavigate('jennifer')
+//     });
+// })();
+//
