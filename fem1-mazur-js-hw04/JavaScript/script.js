3eb41bb44725154384918ffe646956b24a3b05c6
AlinaMgz <alinamgz@ukr.net>
2019-06-23 08:48:44 +0300

    @AlinaMgz fixed js hw 4

--- fem1-mazur-js-hw04/JavaScript/script.js
+++ fem1-mazur-js-hw04/JavaScript/script.js
@@ -1,12 +1,10 @@
-const createNewUser = (userName, userSurname)=> {
-    let newUser = {};
-    userName = prompt('what is your name?', 'Jassie');
-    userSurname = prompt('what is your surname', 'Smith');
-    newUser.firstName= userName;
-    newUser.lastNname = userSurname;
-
+const createNewUser = ()=> {
+    let newUser = {
+    firstName: prompt('what is your name?', 'Jassie'),
+    lastName: prompt('what is your surname', 'Smith'),
+};
     let userLogin = function () {
-      return (`${newUser.firstName.slice(0,1)}${newUser.lastNname}`).toLowerCase();
+      return (`${newUser.firstName.slice(0,1)}${newUser.lastName}`).toLowerCase();
     };
 
     newUser.getLogin = userLogin();
