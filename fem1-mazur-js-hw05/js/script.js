dcf319647f3382654c069bcff0167a11b6808477
AlinaMgz <alinamgz@ukr.net>
2019-06-25 20:45:55 +0300

    @alinamgz finished js-hw-05 and fixed multiple function call

--- fem1-mazur-js-hw05/js/script.js
+++ fem1-mazur-js-hw05/js/script.js
@@ -1,52 +1,55 @@
-const createNewUser = ()=> {
-    let newUser = {
-        firstName: prompt('what is your name?', 'Jassie'),
-        lastName: prompt('what is your surname', 'Smith'),
-        birthday: prompt('when is your day of birth? dd.mm.yyyy', '07.07.1995')
-    };
+function NewUser (firstName, lastName, birthday) {
+    firstName = prompt('what is your name?', 'Jassie');
+    lastName = prompt('what is your surname', 'Smith');
+    birthday = prompt('when is your day of birth? dd.mm.yyyy', '07.07.1995');
+
+    this.getFirstName = firstName;
+    this.getLastName = lastName;
+    this.getBirthday = birthday;
+    this.getLogin = userLogin();
+    this.getAge = userAge();
+    this.getPassword = userPassword();
 
     // getting login for our new user
     function userLogin () {
-        return (`${newUser.firstName.slice(0,1)}${newUser.lastName}`).toLowerCase();
+        return (`${firstName.slice(0,1)}${lastName}`).toLowerCase();
     }
-
 
     //findind out how old is our new user
     function userAge () {
 
+        // receive current date to do calculations + current month and day to check for correct age calculation
         let today = new Date();
         let todayMonth = Number(today.getMonth());
         let todayDay = Number(today.getDate());
 
-        let birthdayArr = newUser.birthday.split('.');
+        // need this array to turn string from prompt into date-object
+        let birthdayArr = birthday.split('.');
 
+        // receive date-of-birth-object + month and day of birth for correct calculations
         let userBirthday = new Date(`${birthdayArr[2]}, ${birthdayArr[1]}, ${birthdayArr[0]}`);
         let birthdayMonth = Number(userBirthday.getMonth());
         let birthdayDay = Number(userBirthday.getDate());
 
+        //lets do some math
         let deltaAge = today.getFullYear() - userBirthday.getFullYear();
-        let age = 0;
 
+        //and check if person already celebrated birthday
         if (birthdayMonth > todayMonth || (birthdayMonth === todayMonth && birthdayDay > todayDay)) {
-            age = Number(deltaAge) - 1;
-        } else {
-            age = 'nope'
+            deltaAge = Number(deltaAge) - 1;
         }
-
-        return age;
+        //done
+        return deltaAge;
     }
-
 
     //creating password for our new user
     function userPassword (){
-        return (`${newUser.firstName.slice(0,1).toUpperCase()}${newUser.lastName.toLowerCase()}${newUser.birthday.slice(-4)}`)
+        return (`${firstName.slice(0,1).toUpperCase()}${lastName.toLowerCase()}${birthday.slice(-4)}`)
     }
+}
 
+let testUser = new NewUser();
 
-    newUser.getLogin = userLogin();
-    newUser.getAge = userAge();
-    newUser.getPassword = userPassword();
-
-    return newUser
-};
-console.log(createNewUser(), createNewUser().getAge, createNewUser().getPassword);
+console.log(`new user is`, testUser,`
+new user is ${testUser.getAge} years old,
+new user's password is ${testUser.getPassword}`);
