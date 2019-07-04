764927f4dc85e0c624d16ae318daae7a9b6616a4
AlinaMgz <alinamgz@ukr.net>
2019-07-04 18:51:27 +0300

    @alinamgz started w320 Speisekarte

--- fem1-mazur-adw-css-hw01/js/script.js
+++ fem1-mazur-adw-css-hw01/js/script.js
@@ -1,6 +1,6 @@
-const menuBtn = document.querySelector('.menu__btn');
+const menuBtn = document.querySelector('.nav-menu__btn');
 const menuBtnImg = document.querySelector('.fas');
-const menu = document.querySelector('.menu');
+const menu = document.querySelector('.nav-menu');
 
 
 menuBtn.addEventListener('click', ()=>{
@@ -8,10 +8,10 @@ menuBtn.addEventListener('click', ()=>{
     if (menuBtnImg.classList.contains('fa-bars')){
         menuBtnImg.classList.remove('fa-bars');
         menuBtnImg.classList.add('fa-times');
-        menu.classList.add('menu--active');
+        menu.classList.add('nav-menu--active');
     } else if (menuBtnImg.classList.contains('fa-times')) {
         menuBtnImg.classList.remove('fa-times');
         menuBtnImg.classList.add('fa-bars');
-        menu.classList.remove('menu--active');
+        menu.classList.remove('nav-menu--active');
     }
 });
